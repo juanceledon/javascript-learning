c2dd45426f8a6649b9821571daf212e9319c013d
Wesley David <nonapeptide@gmail.com>
2018-05-10 17:18:55 -0700

    First blood\!

---
+++ codefights/string_compare.js
@@ -0,0 +1,14 @@
+/*
+  I think I need to for loop through the first one.
+  Then an inner loop to compare the current index to the second string?
+*/
+
+/*
+  function findFirstSubstringOccurrence(s, x) {
+    return s.indexOf(x);
+  }
+*/i
+
+  function findFirstSubstringOccurrence(s, x) {
+    
+}
