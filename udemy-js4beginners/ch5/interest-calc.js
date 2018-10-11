2907a403fd34cc9e7797befe6eb0ff1a5335b669
Wesley David <nonapeptide@gmail.com>
2018-10-10 22:07:31 -0700

    Interest calculator

---
+++ udemy-js4beginners/ch5/interest-calc.js
@@ -0,0 +1,15 @@
+var interest = prompt("What is your interest rate?");
+var amountInvested = prompt("How much money will you invest?");
+var termInMonths = prompt("How many months will your invested last?");
+var amountEarned = 0;
+
+document.write("<table>");
+document.write("<tr><th>Month</th><th>Interest Earned</th><th>Total</th></tr>");
+
+for ( var i = 1; i<=termInMonths; i++) {
+  amountEarned = amountInvested * interest;
+  amountInvested = amountInvested + amountEarned;
+  document.write("<tr><td> " + i + " </td><td> " + amountEarned + " </td><td> " + amountInvested + " </td></tr>");
+}
+
+document.write("</table>");
