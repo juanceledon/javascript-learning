a31d6bc407b993fc0c91da0db7580f608727335b
Wesley David <nonapeptide@gmail.com>
2018-10-11 15:17:14 -0700

    Starting to play with events.

---
+++ udemy-js4beginners/ch6/functions.js
@@ -0,0 +1,17 @@
+// chapter 6, exercise 1
+
+function english() {
+ alert("Hello! Pleased to meet you.");
+}
+
+function spanish() {
+ alert("Hola. Como estas?");
+}
+
+function hebrew() {
+ alert("Shalom!");
+}
+
+function french() {
+ alert("Bon Jour!");
+}
